e634aabd4cfed4097fb239fb3deed326331697f7
tommy <tommy5566123@gmail.com>
2021-12-02 15:29:17 +0800

    Initial Commit

---
+++ JohnTea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JohnTea
+//
+//  Created by 蔡尚諺 on 2021/12/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
